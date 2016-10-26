0c36cefbaa62f65601b30d3a912989fe3bf54059
Lucía Rodrigo Insausti <emera86@gmail.com>
2016-10-26 10:16:04 +0200

    'win'

---
+++ under_construction/js/timeless.js
@@ -0,0 +1,31 @@
+/* Timeless Under Construction Template by Web Tech Gadgetry */
+
+// Background - only one set to true
+
+var useSlideShow = true;
+var ytPlayer = false;
+
+// Slideshow Variables
+// Edit these to change images and slide duration
+
+var slideDuration = 4600;
+var fadeDuration = 1000;
+
+// Turn animations on or off
+
+var animations = true;
+
+// Use sidebar
+
+var sidebar = true;
+
+// Array of images to be loaded into the slideshow
+// Use only one image to have a static background
+
+var images = ["img/background1.jpg", "img/background2.jpg", "img/background3.jpg"];
+
+var videoURL = "https://www.youtube.com/watch?v=zYninonj4g4";
+
+// Date to countdown to
+
+var countdownDate = "25 december 2016 12:00:00"; // Enter date in format: DD (full month) YYYY HH:MM:SS
